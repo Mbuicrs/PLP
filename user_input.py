1fd606f557b0d064b54182e0c18e01e53806bb13
Cyrus <mbuicyrus@gmail.com>
2024-02-20 23:49:41 +0300

    First commit

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = str(input("Please enter your name:"))    # Asks for the name
+age = int(input("Enter your age: "))            #Asks for the age
+location = str(input("Enter your addres: "))    #Asks for the location address
+
+print("Good morning {}, please confirm your age is {} years and you live in {}".format(name, age, location))
+
